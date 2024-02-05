8d8a840ee7a7d88b5fde1c17c0c40659108179df
DJI LALI <bentaalla.djillali@outlook.com>
2024-02-05 13:45:10 +0100

    Mise à jour

--- config/connection.php
+++ config/connection.php
@@ -1,16 +1,15 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname="travel_db";
+// Paramètres de connexion à la base de données
+define('DB_SERVER', 'localhost'); // Adresse du serveur de base de données
+define('DB_USERNAME', 'root'); // Nom d'utilisateur pour se connecter à la base de données
+define('DB_PASSWORD', ''); // Mot de passe pour se connecter à la base de données
+define('DB_NAME', 'travel_db'); // Nom de la base de données
 
+// Tentative de connexion à la base de données MySQL
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-$conn = mysqli_connect($servername, $username, $password,$dbname);
-
-if (!$conn) 
-{
-    die("Connection failed: " . mysqli_connect_error());
+// Vérifier la connexion
+if($conn === false){
+    die("ERROR: Could not connect. " . $conn->connect_error);
 }
-
-?>
